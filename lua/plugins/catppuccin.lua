5a8f7acae719868ae435a775bc4dac534f234504
Ryan Gloff <ryan@ryangloff.com>
2024-02-08 20:29:35 -0500

    Changed nvim to transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,13 @@
 return {
   "catppuccin/nvim",
   lazy = false,
-  name = "catppuccin", 
+  name = "catppuccin",
   priority = 1000,
   config = function()
+    require('catppuccin').setup({
+      transparent_background = true
+    })
+
     vim.cmd.colorscheme "catppuccin"
   end
 }
