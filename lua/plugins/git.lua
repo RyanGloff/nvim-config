ac54175fb6f9919c8ccd13b3351494e0448c569e
Ryan Gloff <gloffrm@amazon.com>
2024-10-31 18:31:11 +0000

    Added more Gitsigns commands to vim config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,9 +4,15 @@ return {
     require('gitsigns').setup()
 
     vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+    vim.keymap.set('n', '<leader>gd', ':Gitsigns diffthis<CR>', {})
+    vim.keymap.set('n', '<leader>gi', ':Gitsigns preview_hunk_inline<CR>', {})
     vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+    vim.keymap.set('n', '<leader>gb', ':Gitsigns blame_line<CR>', {})
     vim.keymap.set('n', '<leader>gs', ':Gitsigns stage_hunk<CR>', {})
     vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>', {})
     vim.keymap.set('n', '<leader>gn', ':Gitsigns next_hunk<CR>', {})
+
+    -- Turn on highlighting on the line numbers too
+    vim.cmd(':Gitsigns toggle_numhl');
   end
 }
